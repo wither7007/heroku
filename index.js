3df3287a67af095fbadbedf9f46124d2f02e39cd
jim steffes <jimsteffes@outlook.com>
2021-12-10 17:46:21 -0600

    ok

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express=require("express")
 const app =express()
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Greetings from Smith Lodge')
   });
 app.listen(process.env.PORT||5000)
