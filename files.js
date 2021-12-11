b5129cde5c4a0f4ffca895b11fb2d6fb06f5caef
jim steffes <jimsteffes@outlook.com>
2021-12-10 22:16:36 -0600

    ok

---
+++ files.js
@@ -0,0 +1,34 @@
+[
+  {
+    "Name": "node_modules",
+    "LastWriteTime": "2021-12-10T21:24:54.9605428-06:00"
+  },
+  {
+    "Name": "Node-Heroku-Deployment",
+    "LastWriteTime": "2021-12-10T14:10:06.2040711-06:00"
+  },
+  {
+    "Name": "public",
+    "LastWriteTime": "2021-12-10T21:34:05.4222068-06:00"
+  },
+  {
+    "Name": ".gitignore",
+    "LastWriteTime": "2021-12-10T15:40:22.76057-06:00"
+  },
+  {
+    "Name": "files.js",
+    "LastWriteTime": "2021-12-10T22:02:42.1866524-06:00"
+  },
+  {
+    "Name": "index.js",
+    "LastWriteTime": "2021-12-10T21:39:45.8192794-06:00"
+  },
+  {
+    "Name": "package-lock.json",
+    "LastWriteTime": "2021-12-10T21:24:55.4962374-06:00"
+  },
+  {
+    "Name": "package.json",
+    "LastWriteTime": "2021-12-10T21:27:06.3734683-06:00"
+  }
+]
